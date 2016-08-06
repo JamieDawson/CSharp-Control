2877dbfe634eed87ed8243e8af1153bdf56d31c5
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-08-05 21:05:54 -0400

    A control I've created.

---
+++ C_CreatingControl/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace C_CreatingControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myButton1 = new C_CreatingControl.MyButton();
+            this.SuspendLayout();
+            // 
+            // myButton1
+            // 
+            this.myButton1.BackColor = System.Drawing.SystemColors.Control;
+            this.myButton1.ButtonText = "Jamie\'s Button";
+            this.myButton1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.myButton1.Location = new System.Drawing.Point(128, 76);
+            this.myButton1.Name = "myButton1";
+            this.myButton1.Size = new System.Drawing.Size(110, 52);
+            this.myButton1.TabIndex = 0;
+            this.myButton1.Load += new System.EventHandler(this.myButton1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(380, 226);
+            this.Controls.Add(this.myButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MyButton myButton1;
+    }
+}
+
---
+++ C_CreatingControl/MyButton.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_CreatingControl
+{
+    public partial class MyButton : UserControl
+    {
+        public MyButton()
+        {
+            InitializeComponent();
+        }
+        string text = "";
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            DrawButton(Color.FromKnownColor(KnownColor.Control));
+        }
+
+        public string ButtonText
+        {
+            get { return text; }
+            set { text = value; }
+        }
+
+        private void MyButton_MouseHover(object sender, EventArgs e)
+        {
+            Color myColor = Color.FromArgb(225, Color.FromKnownColor(KnownColor.Control).R - 30, Color.FromKnownColor(KnownColor.Control).R - 5, 255);
+            DrawButton(myColor);
+        }       
+        void DrawButton(Color c)
+        {
+            SolidBrush s = new SolidBrush(c);
+            Graphics g = this.CreateGraphics();
+            g.FillRectangle(s, 0, 0, this.Width, this.Height);
+            s.Color = Color.FromArgb(225, c.R - 13, c.G - 13, c.B - 13);
+            g.FillRectangle(s, 0, this.Height / 2, this.Width, this.Height / 2);
+            PointF fpoint = new Point((this.Width / 2) - (text.Length - 5), (this.Height / 2) - (text.Length - 5));
+            FontFamily ff = new FontFamily("Arial");
+            Font f = new System.Drawing.Font(ff, 8);
+            s.Color = Color.Black;
+            g.DrawString(text, f, s, fpoint);
+        }
+
+        private void MyButton_MouseLeave(object sender, EventArgs e)
+        {
+            DrawButton(Color.FromKnownColor(KnownColor.Control));
+        }
+
+        private void MyButton_MouseEnter(object sender, EventArgs e)
+        {
+            Color myColor = Color.FromArgb(225, Color.FromKnownColor(KnownColor.Control).R - 30, Color.FromKnownColor(KnownColor.Control).R - 5, 255);
+            DrawButton(myColor);
+        }
+        //Changes the color of the button to yellow when clicked.
+        private void MyButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            Color myColor = Color.FromArgb(255, Color.FromKnownColor(KnownColor.Control).R + 15, Color.FromKnownColor(KnownColor.Control).G - 15, 150);
+            DrawButton(myColor);
+        }
+    }
+}
+
